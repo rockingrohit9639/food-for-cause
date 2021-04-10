9852657f37ddabfdce6c2cbd46808e6506f9b45b
BeatTheVirusStartup <beatthevirusapp@gmail.com>
2021-04-10 17:27:09 +0530

    Added Hero Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HeroComp from "./Components/HeroComp";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/"> <Navbar></Navbar> </Route>
+          <Route exact path="/"> 
+            <Navbar />
+            <HeroComp /> 
+          </Route>
         </Switch>
       </Router>
     </div>
