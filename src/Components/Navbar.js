234b5c369e57b17c55786320a3f958c6c7ae5bc8
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2021-04-10 16:01:26 +0530

    added navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,18 +4,132 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="navbar__left">
-        <input type="text" />
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <button
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="block h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+              <svg
+                className="hidden h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex-shrink-0 flex items-center">
+              <h1 className="text-xl text-white font-bold"> Food For Cause </h1>
+            </div>
+            <div className="hidden sm:block sm:ml-6">
+              <div className="flex space-x-4">
+                <Link
+                  to="/"
+                  className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                  aria-current="page"
+                >
+                  Home
+                </Link>
+
+                <Link
+                  to="/ngo"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  NGOs
+                </Link>
+
+                <Link
+                  to="/restaurants"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Restaurants
+                </Link>
+
+                <Link
+                  Link="/about"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  AboutUs
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <div className="ml-3 relative">
+              <div>
+                <Link
+                  to="/donate"
+                  className="text-white border-2 rounded-lg border-red-50 border-opacity-50 pl-4 pr-4 pt-2 pb-2"
+                >
+                  Donate
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="navbar__logo">
-        <h2 className="font-bold"> Food For Cause <span>.</span>
-        </h2>
-      </div>
-      <div className="navbar__right">
-        <Link to="#"> AboutUs </Link>
-        <Link to="#"> SignIn </Link>
-        <Link to="#"> Donate </Link>
+
+      <div className="sm:hidden" id="mobile-menu">
+        <div className="px-2 pt-2 pb-3 space-y-1">
+          <Link
+            to="/"
+            className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+            aria-current="page"
+          >
+            Home
+          </Link>
+
+          <Link
+            to="/ngo"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+          >
+            NGOs
+          </Link>
+
+          <Link
+            to="/restaurants"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+          >
+            Restaurants
+          </Link>
+
+          <Link
+            Link="/about"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+          >
+            AboutUs
+          </Link>
+        </div>
       </div>
     </nav>
   );
